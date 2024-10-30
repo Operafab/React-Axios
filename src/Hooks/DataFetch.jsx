83fd0ebc92c93e829832b29cc7f66f60213a017d
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-10-30 20:52:05 +0100

    adjustments

--- src/Hooks/DataFetch.jsx
+++ src/Hooks/DataFetch.jsx
@@ -25,23 +25,24 @@ const DataFetch = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-8 text-red-400">Users Information</h1>
-      {loading && <p className="text-lg font-semibold text-center">Loading...</p>}
+      {loading && <p className="text-lg font-semibold text-center text-lime-700">Loading...</p>}
       {error && <p className="text-red-500 text-center">{error}</p>}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {data.map((user, index) => (
-          <div key={index} className="bg-white shadow-md rounded-lg p-6">
+        {data.map((user) => (
+          <div key={user} className="bg-slate-400 shadow-md rounded-lg p-6 hover:shadow-black cursor-pointer">
             <img
               src={user.picture.medium}
               alt={`User picture`}
               className="w-24 h-24 rounded-full mx-auto mb-4"
+    
             />
-            <h2 className="text-lg font-semibold text-center">
+            <h1 className="text-lg font-semibold text-center">
               {user.name.title} {user.name.first} {user.name.last}
-            </h2>
+            </h1>
             <p className="text-center text-gray-700 mb-2">{user.email}</p>
             <p className="text-center text-gray-700 mb-2">{user.phone}</p>
-            <p className="text-center text-gray-500">
+            <p className="text-center text-gray-200">
               {user.location.street.number} {user.location.street.name}, {user.location.country}
             </p>
           </div>
